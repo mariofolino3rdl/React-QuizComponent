5690655a609b578fd11fd29d9e5b76e00901b4d6
Mario Folino <mfolino@pls3rdlearning.com>
2018-10-01 13:58:51 -0400

    Next module.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,13 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component
 {
+    handleClick(buttonText)
+    {
+        if(buttonText === this.props.quiz_question.answer)
+        {
+            this.props.showNextQuestionHandler()
+        }
+    }
     render()
     {
         return (
@@ -11,8 +18,11 @@ class QuizQuestion extends Component
                     <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
+
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+                        {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                            return <QuizQuestionButton key={index} button_text={this.props.quiz_question.answer_options[0]} clickHandler={this.handleClick.bind(this)} />
+                        })}
                     </ul>
                 </section>
             </main>
