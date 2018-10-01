ba751e439c933abfdbf7b15b6b26ec80c631b52c
Mario Folino <mfolino@pls3rdlearning.com>
2018-10-01 15:09:11 -0400

    Added reset handler

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,6 +6,11 @@ let quizData = require('./quiz_data.json')
 
 class Quiz extends Component
 {
+    handleResetClick()
+    {
+        this.setState({quiz_position: 1})
+    }
+
     showNextQuestion()
     {
         this.setState((state) => {return {quiz_position: state.quiz_position + 1}})
@@ -15,7 +20,7 @@ class Quiz extends Component
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
             <div>
-                {isQuizEnd ? <QuizEnd /> : 
+                {isQuizEnd ? <QuizEnd resetClickHandler="this.handleResetClick.bind(this)" /> : 
                     <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
                 }
             </div>
